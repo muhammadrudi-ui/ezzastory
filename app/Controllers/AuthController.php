52ba9882f109b4eb8f9fe4c824643645796cf0e7
Muhammad Rudi <rudibwi777@gmail.com>
2025-05-05 01:10:06 +0700

    nyoba lagi

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -13,7 +13,6 @@ class AuthController extends BaseController
     protected $session;
     protected $request;
 
-    
     public function __construct()
     {
         $this->userModel = new UserModel();
