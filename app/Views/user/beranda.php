15c9f775928cbd4b027a5f66a2c661bf02f78060
Muhammad Rudi <rudibwi777@gmail.com>
2025-05-07 22:35:49 +0700

    porto beranda done bug

--- app/Views/user/beranda.php
+++ app/Views/user/beranda.php
@@ -227,7 +227,7 @@
                         <div class="col-md-4">
                             <a href="<?= base_url('user/portofolio/detail/' . $item['id']) ?>" class="text-decoration-none">
                                 <div class="card">
-                                    <img src="<?= base_url('uploads/portofolio/' . $item['foto_utama']) ?>" class="card-img-top"
+                                    <img src="<?= base_url($item['foto_utama']) ?>" class="card-img-top"
                                         alt="<?= esc($item['nama_mempelai']) ?>">
                                     <div class="card-body text-center">
                                         <h5 class="card-title"><?= esc($item['nama_mempelai']) ?></h5>
